e27fa921b629dd637e2940e20a40d7a2c69db394
Alchimis <79978300+Alchimis@users.noreply.github.com>
2024-12-10 21:33:43 +0300

    Написал миграцию для таблицы Пользователь

---
+++ migrations/m241210_132504_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m241210_132504_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(255)->unique()->notNull(),
+            'email' => $this->string(255)->unique()->notNull(),
+            'password' => $this->string(255)->notNull(),
+            'role' => $this->string(63)->notNull(),
+            'createdAt' => $this->timestamp()->defaultExpression('now()'), 
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
